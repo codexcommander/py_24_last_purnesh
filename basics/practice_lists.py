2a81201bda725a8e30d1c41fa5b068763d7dfd80
Lancaster456 <150602029+Lancaster456@users.noreply.github.com>
2024-11-21 08:36:09 +0530

    lists task completed

--- basics/practice_lists.py
+++ basics/practice_lists.py
@@ -41,3 +41,27 @@ c.pop(-1)
 
 f = c.pop(-1)
 print(c)
+
+#sorting for only int, only strings
+numbers = [4, 2, 9, 1]
+numbers.sort()
+print(numbers)
+
+numbers_tuple = (6, 9, 3, 1)
+numbers_set = {5, 5, 10, 1, 0}
+numbers_tuple_sorted = sorted(numbers_tuple)
+numbers_set_sorted = sorted(numbers_set)
+print(numbers_tuple_sorted)
+print(numbers_set_sorted)
+
+l =['abc' , 'cd' , 'xy' , 'ba' , 'dc']
+l.sort()
+print(l)
+
+l7 = [3, 7, 5, 9, 2, 1, 4]
+l7.insert(-8, 10)
+print(l7)
+
+l7.insert(-7, ["hello", 4444, 555, 666])
+print(l7)
+
