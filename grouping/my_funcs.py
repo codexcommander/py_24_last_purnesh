7a81277ca4d4de91334d148f8e3d77fc4a01b6b3
Lancaster456 <150602029+Lancaster456@users.noreply.github.com>
2024-12-01 22:30:28 +0530

    funcs

---
+++ grouping/my_funcs.py
@@ -0,0 +1,63 @@
+def py():
+    for i in range(40,80):
+       print(i)
+py()
+
+def pt():
+    pt_li=[]
+    for i in range(30):
+        pt_li.insert(0,i)
+    print(pt_li)
+print(pt())
+
+#false case:
+def pt():
+    pt_li=[]
+    for i in range(30):
+        pt_li.insert(0,i)
+print(pt())
+
+
+def pt():
+    pt_li=[]
+    for i in range(30):
+        pt_li.append(i)
+    return pt_li
+
+print(pt())
+
+def js():
+    js_li=[]
+    for i in range(20,50,3):
+        js_li.append(i)
+    return js_li
+
+rig=js()
+print(rig)
+rig.append(12345)
+print(rig)
+
+def usp():
+    start=input("enter first input:")
+    end=input("enter second input:")
+    usp_li=[]
+    for i in range(int(start),int(end)):
+        usp_li.append(i)
+    return usp_li
+
+print(usp())
+
+def usd():
+    start=input("enter first input:")
+    end=input("enter second input:")
+    if start.isnumeric():
+        start=int(start)
+    if end.isnumeric():
+        end=int(end)
+    usd_li=[]
+    for i in range(int(start),int(end)):
+        usd_li.append(i)
+    return usd_li
+
+print(usd())
+
